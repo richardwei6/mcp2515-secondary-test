1635d5fae5f757534c2439d958eff81e6e0c3371
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2023-05-07 00:12:34 -0700

    Comment out msg skipped

--- sketch_may5a/sketch_may5a.ino
+++ sketch_may5a/sketch_may5a.ino
@@ -12,6 +12,7 @@ void setup() {
 }
 
 struct can_frame frame;
+//int prevData = -1;
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -21,17 +22,26 @@ void loop() {
   if (mcp2515.readMessage(&frame) == MCP2515::ERROR_OK && frame.can_id == 1){
     Serial.print("recv can req frame; data = ");
     Serial.println(frame.data[0]);
+
+    /*if (prevData != -1 && frame.data[0] - 1 != prevData && !(frame.data[0] == 0 && prevData == 255)){
+      Serial.print("MESSAGE SKIPPED = ");
+      Serial.print(prevData);
+      Serial.print(" : ");
+      Serial.println(frame.data[0]);
+    }
     
+    prevData = frame.data[0];*/
+
     //
 
     frame = can_frame();
 
     frame.can_id = 2;
     frame.can_dlc = 1;
-    frame.data[0] = 2;
+    frame.data[0] = 5;
 
     mcp2515.sendMessage(&frame);
   }
 
-  delay(50);  
+  delay(20);  
 }
